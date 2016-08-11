d89b14b9294e149826097ab49225626fe4304e2d
THACHER <thachert1d@gmail.com>
2016-08-10 20:03:33 -0700

    other things

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ angular
         url: '/',
         templateUrl: 'pages/home.html'
       })
-      .state('notHome', {
-        url: '/notHome',
-        templateUrl: 'pages/notHome.html'
+      .state('button1', {
+        url: '/button1',
+        templateUrl: 'pages/button1.html'
       })
   }])
